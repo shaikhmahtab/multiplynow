420711dbebdd9009f31cbe486aa64b76e64ab117
shaikhmahtab <58741836+shaikhmahtab@users.noreply.github.com>
2023-09-17 12:17:41 -0400

    index css and js for multiplication api

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const num1Input = document.getElementById("num1");
+    const num2Input = document.getElementById("num2");
+    const calculateButton = document.getElementById("calculate");
+    const resultDiv = document.getElementById("result");
+
+    calculateButton.addEventListener("click", async () => {
+        const num1 = parseFloat(num1Input.value);
+        const num2 = parseFloat(num2Input.value);
+
+        if (isNaN(num1) || isNaN(num2)) {
+            resultDiv.textContent = "Please enter valid numbers.";
+        } else {
+            try {
+                const response = await fetch("https://mahtabnodejs.azurewebsites.net/api/multiplication?", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ num1, num2 }),
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    resultDiv.textContent = `Result: ${data}`;
+                } else {
+                    resultDiv.textContent = "Error calculating the result.";
+                }
+            } catch (error) {
+                console.error("An error occurred:", error);
+                resultDiv.textContent = "An error occurred. Please try again later.";
+            }
+        }
+    });
+});
